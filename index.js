c79cfe5c86cd2bed43300958bc9d1012351aec22
pranavkavade16 <pranavkavade69739@gmail.com>
2025-11-27 12:44:58 +0530

    Feat: adding the delete api

--- index.js
+++ index.js
@@ -288,13 +288,13 @@ app.post('/sneakers/cart/:sneakerId', async (req, res) => {
 });
 
 // API to delete a sneaker from cart
-app.delete('/sneakers/cart/delete/:sneakerId', async (req, res) =>{
+app.delete('/sneakers/cart/delete/:cartId', async (req, res) =>{
     try {
         
        const { quantity } = req.body;
 
     if (quantity === 1) {
-      const deleted = await Cart.findByIdAndDelete(req.params.sneakerId);
+      const deleted = await Cart.findByIdAndDelete(req.params.cartId);
 
       if (!deleted) {res.status(404).json({ error: "Sneaker not found." });}
 
